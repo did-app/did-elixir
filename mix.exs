0b22cf1a54f5a08e99f46a9016dd8633c7c8d4e7
Peter Saxton <peterhsaxton@gmail.com>
2019-12-30 16:12:36 +0000

    setup for hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Kno.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/trykno/kno-elixir",
+      homepage_url: "https://trykno.com"
     ]
   end
 
@@ -19,8 +23,27 @@ defmodule Kno.MixProject do
 
   defp deps do
     [
+      {:jason, "~> 1.0"},
       {:plug, "~> 1.8", optional: true},
-      {:phoenix_html, "~> 2.13", optional: true}
+      {:phoenix_html, "~> 2.13", optional: true},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    This simplest way to add passwordless authentication to your application.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Peter Saxton"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/trykno/kno-elixir",
+        "Homepage" => "https://trykno.com"
+      }
     ]
   end
 end
